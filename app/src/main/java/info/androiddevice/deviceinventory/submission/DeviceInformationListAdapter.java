9234e7c4e0cf155497a4639be7946ab61fd0a03e
Jared <jared.getzoff@gmail.com>
2014-08-22 12:57:20 -0700

    List optimizations: truncate long properties and expand on click, Device name input box, fixes for 2.2+ devices including crash for referencing color that only existed in v13 and fixes to display action bar title and icons properly

--- app/src/main/java/info/androiddevice/deviceinventory/submission/DeviceInformationListAdapter.java
+++ app/src/main/java/info/androiddevice/deviceinventory/submission/DeviceInformationListAdapter.java
@@ -1,6 +1,7 @@
 package info.androiddevice.deviceinventory.submission;
 
 import android.content.Context;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,14 +13,18 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class DeviceInformationListAdapter extends BaseAdapter {
+    public static final int MAX_LINES = 10;
 
     private final LayoutInflater mInflater;
     private final int mWhiteColor;
     private final int mGreyColor;
     private List<KeyValuePair> mItems;
+    private String mDeviceName;
 
     public DeviceInformationListAdapter(Context context, JSONObject deviceInformation){
         mInflater = LayoutInflater.from(context);
@@ -31,14 +36,22 @@ public class DeviceInformationListAdapter extends BaseAdapter {
             try {
                 final String key = keys.getString(ii);
                 final Object value = deviceInformation.get(key);
-                mItems.add(new KeyValuePair(key, value));
+                final String valueString = String.valueOf(value);
+                mItems.add(new KeyValuePair(key, value, valueString, key.equals("name")));
             } catch (JSONException e) {
                 e.printStackTrace();
             }
-
         }
 
-
+        Collections.sort(mItems, new Comparator<KeyValuePair>() {
+            @Override
+            public int compare(KeyValuePair lhs, KeyValuePair rhs) {
+                if(lhs.isEditable){
+                    return -1;
+                }
+                return lhs.key.compareTo(rhs.key);
+            }
+        });
     }
 
     @Override
@@ -56,11 +69,16 @@ public class DeviceInformationListAdapter extends BaseAdapter {
         return 0;
     }
 
+//    @Override
+//    public boolean isEnabled(int position) {
+//        return getItem(position).isEditable;
+//    }
+
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         View view = convertView;
         if(null == view){
-            view = mInflater.inflate(android.R.layout.simple_list_item_2, parent, false);
+            view = mInflater.inflate(R.layout.editable_list_item, parent, false);
         }
         KeyValuePair pair = getItem(position);
         if(null != pair){
@@ -69,8 +87,22 @@ public class DeviceInformationListAdapter extends BaseAdapter {
             key.setTextColor(mWhiteColor);
 
             final TextView value = ((TextView)view.findViewById(android.R.id.text2));
-            value.setText(String.valueOf(pair.value));
-            value.setTextColor(mGreyColor);
+            final TextView editable  = ((TextView)view.findViewById(R.id.editable));
+
+            if(pair.isEditable){
+                value.setVisibility(View.GONE);
+                editable.setVisibility(View.VISIBLE);
+                if(null != mDeviceName && mDeviceName.length() > 0) {
+                    editable.setText(mDeviceName);
+                }
+            } else {
+                value.setVisibility(View.VISIBLE);
+                editable.setVisibility(View.GONE);
+                value.setText(pair.valueString);
+                value.setTextColor(mGreyColor);
+                value.setMaxLines(10);
+                value.setEllipsize(TextUtils.TruncateAt.END);
+            }
 
             //details.setVisibility(TextView.GONE);
 
@@ -89,12 +121,22 @@ public class DeviceInformationListAdapter extends BaseAdapter {
         return view;
     }
 
-    static class KeyValuePair{
+    public void setDeviceName(String deviceName) {
+        mDeviceName = deviceName;
+        notifyDataSetChanged();
+    }
+
+    static class KeyValuePair {
         String key;
         Object value;
-        KeyValuePair(String key, Object value){
+        String valueString;
+        boolean isEditable = false;
+
+        KeyValuePair(String key, Object value, String valueString, boolean isEditable){
             this.key = key;
             this.value = value;
+            this.valueString = valueString;
+            this.isEditable = isEditable;
         }
     }
 }
